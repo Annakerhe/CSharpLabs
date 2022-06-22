04438d67b2930efe67724f65bba030ac118f7933
Annenkirche <ms.irbis69@gmail.com>
2022-06-22 19:28:41 +0300

    changed: lab2

---
+++ labs/lab2/NetworkModel/SpeedNetwork.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace NetworkModel
+{
+    public class SpeedNetwork : PCNetwork
+
+        {
+            private double P { get; set; }
+            public SpeedNetwork(string name, int stations, double dist, double speed)
+                : base(name, stations, dist)
+            {
+                P = speed;
+            }
+            public override double GetQuality()
+            {
+                return StationsCount * P;
+            }
+        }
+    }
+
